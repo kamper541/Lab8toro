cf0cba4d8f9090292dfdd3d13f8b405f8d6cc600
kamper541 <kamper543@gmail.com>
2020-03-24 21:48:35 +0700

    aSaasdsad

---
+++ build/precache-manifest.66d26cfc33b881a561bd47d03e260f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec84e084389c2693ecdabb698f8781b0",
+    "url": "/Lab8toro/index.html"
+  },
+  {
+    "revision": "53619d05ac797b1a2bc5",
+    "url": "/Lab8toro/static/css/main.04418a4e.chunk.css"
+  },
+  {
+    "revision": "fb993ccb3d4d3b32684b",
+    "url": "/Lab8toro/static/js/2.06698018.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/Lab8toro/static/js/2.06698018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53619d05ac797b1a2bc5",
+    "url": "/Lab8toro/static/js/main.34839e41.chunk.js"
+  },
+  {
+    "revision": "df221fcdbb970bc164a2",
+    "url": "/Lab8toro/static/js/runtime-main.db9879c2.js"
+  }
+]);
